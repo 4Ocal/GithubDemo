0b1cea735c4d933aa539a739066cb95f2e6f982f
Calvin Chu <Calvin@c.local>
2017-03-01 23:46:31 -0500

    filtering by language is working

--- GithubDemo/SearchSettingsViewController.swift
+++ GithubDemo/SearchSettingsViewController.swift
@@ -20,33 +20,26 @@ class SearchSettingsViewController: UITableViewController {
     @IBOutlet weak var rubyCell: UITableViewCell!
     @IBOutlet weak var swiftCell: UITableViewCell!
     
-    var checked: [Bool] = []
     var settings: GithubRepoSearchSettings?
 
     weak var delegate: SettingsPresentingViewControllerDelegate?
     
+    var languages = ["Java", "JavaScript", "Objective-C", "Python", "Ruby", "Swift"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         
-        javaCell.textLabel?.text = "Java"
-        javascriptCell.textLabel?.text = "JavaScript"
-        objectivecCell.textLabel?.text = "Objective-C"
-        pythonCell.textLabel?.text = "Python"
-        rubyCell.textLabel?.text = "Ruby"
-        swiftCell.textLabel?.text = "Swift"
+        let numberOfRows = tableView.numberOfRows(inSection: 1)
+        for row in 1...(numberOfRows-1) {
+            let cell = tableView.cellForRow(at: IndexPath(row: row, section: 1))
+            cell?.textLabel?.text = languages[row-1]
+        }
         
         slider.value = Float(settings!.minStars)
         slider.sendActions(for: UIControlEvents.valueChanged)
-        langSwitch.isOn = Bool(settings!.langSwitch)
-        langSwitch.sendActions(for: UIControlEvents.valueChanged)
+        langSwitch.setOn((settings?.langSwitch)!, animated: true)
         if langSwitch.isOn {
-            for row in 1...tableView.numberOfRows(inSection: 1) {
+            for row in 1...(numberOfRows-1) {
                 let cell = tableView.cellForRow(at: IndexPath(row: row, section: 1))
                 let text = (cell?.textLabel?.text)!
                 if (settings?.languages.contains(text))! {
@@ -133,51 +126,6 @@ class SearchSettingsViewController: UITableViewController {
             swiftCell.isHidden = true
         }
     }
-    
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
